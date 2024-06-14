0569385c112d56b4fab057df7cbd201d1b23817f
roshith2001 <roshithj@gmail.com>
2024-06-14 22:09:56 +0530

    fix-menu

--- app/components/MenuDrawer.js
+++ app/components/MenuDrawer.js
@@ -1,8 +1,14 @@
 import MenuItems from "./MenuItems";
+import { useTheme } from "next-themes";
 
 export default function MenuDrawer() {
+
+    const{theme} = useTheme();
+
     return (
-        <div className="w-screen h-screen z-20 absolute top-0 left-0 flex flex-col pl-14 justify-center gap-12 bg-black/90">
+        <div className={`w-screen h-screen 
+        ${theme=='dark'?'bg-black/90':'bg-white/90'}
+        z-20 absolute top-0 left-0 flex flex-col pl-14 justify-center gap-12`}>
             <MenuItems />
         </div>
     );
