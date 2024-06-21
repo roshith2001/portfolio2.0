0e3d7b5cbbae3ab60354166e66b1d2fb364df29b
roshith2001 <roshithj@gmail.com>
2024-06-21 19:55:01 +0530

    fix

--- utils/contactDetails.js
+++ utils/contactDetails.js
@@ -8,7 +8,7 @@ const contacts = [{
   },
   {
     "text": '9746087500',
-    "link": 'https://wa.me/9746087500?text=urlencodedtext',
+    "link": 'https://wa.me/9746087500?text=Hi,Let_us_connect!',
     "icon": <FaWhatsapp/>
   },
 ]
